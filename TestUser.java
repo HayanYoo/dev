b56c101b29bf8f0b1dcc961ed68f0f21b8afb7cf
HayanYoo <73471529+HayanYoo@users.noreply.github.com>
2020-10-26 19:56:02 +0900

    Add files via upload

---
+++ TestUser.java
@@ -0,0 +1,11 @@
+
+public class TestUser {
+	public static void main(String[] args) {
+		String name = "HongGilDong";
+		int age = 35;
+		
+		User us = new User();
+		us.printData(name, age);
+
+	}
+}
